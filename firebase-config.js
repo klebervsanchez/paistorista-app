8287846ddfac5568cf3caf75bbf7b073507d1381
KLEBER SANCHEZ <klebervsanchez82@gmail.com>
2025-11-09 20:18:54 -0300

    melhorias propostas V2

--- firebase-config.js
+++ firebase-config.js
@@ -3,10 +3,11 @@ const firebaseConfig = {
   apiKey: "AIzaSyCT9QQ9-AUvngXp0c_7l9yQXpDk1seAgmw",
   authDomain: "paistorista-57706.firebaseapp.com",
   projectId: "paistorista-57706",
-  storageBucket: "paistorista-57706.firebasestorage.app",
+  storageBucket: "paistorista-57706.appspot.com",
   messagingSenderId: "934152031761",
   appId: "1:934152031761:web:f0182855df02966e462bb0",
+  measurementId: "G-X4KZM1NZ83"
 };
 
 firebase.initializeApp(firebaseConfig);
-
+const db = firebase.firestore();
