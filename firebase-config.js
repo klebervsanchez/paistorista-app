241a4afeac519096023d67c7f42d9255e7af895d
KLEBER SANCHEZ <klebervsanchez82@gmail.com>
2025-11-09 08:23:09 -0300

    Refactor firebase-config.js structure

--- firebase-config.js
+++ firebase-config.js
@@ -5,8 +5,7 @@ const firebaseConfig = {
   projectId: "paistorista-57706",
   storageBucket: "paistorista-57706.firebasestorage.app",
   messagingSenderId: "934152031761",
-  appId: "1:934152031761:web:f0182855df02966e462bb0",
-  measurementId: "G-X4KZM1NZ83"
+  appId: ""1:934152031761:web:f0182855df02966e462bb0",
 };
 
 firebase.initializeApp(firebaseConfig);
